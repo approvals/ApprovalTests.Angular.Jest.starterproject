34b9ba7b72debea75feac191f1cc70db2138ff93
Lada Kesseler <lada.kesseler1@t-mobile.com>
2023-09-07 20:21:39 -0700

    ! F adding approvals replicates the issue

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,13 @@
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect } from "@jest/globals";
+import {verify} from "approvals/lib/Providers/Jest/JestApprovals";
 
 describe('ApprovalsStuff', () => {
 
   it('should approve', () => {
     expect("5").toBe("5");
+  });
+
+  it('should approve with approvals', () => {
+    verify('foo');
   });
 });
