7bbeaaa9d9eb852b0a7b74f23140695188ae7b3a
Pedro Calixto <Pedro.Calixto1@T-Mobile.com>
2023-09-08 19:41:49 -0400

    . t added angular component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,8 @@
 import { describe, expect } from "@jest/globals";
-import {verify} from "approvals/lib/Providers/Jest/JestApprovals";
+import { verify } from "approvals/lib/Providers/Jest/JestApprovals";
+import { Options } from "approvals/lib/Core/Options";
+import { AppComponent } from "./app.component";
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 describe('ApprovalsStuff', () => {
 
@@ -9,5 +12,29 @@ describe('ApprovalsStuff', () => {
 
   it('should approve with approvals', () => {
     verify('foo');
+  });
+});
+
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AppComponent ],
+      // ... any other necessary modules or providers
+    })
+    .compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should render the expected HTML', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    const htmlContent = compiled.outerHTML;
+    // Your test assertion. For instance:
+    const options = new Options().forFile().withFileExtention(".html")
+    verify(htmlContent, options);    
   });
 });
